65af2eb84c49f5e6ae563b581ec4936ce4f477a0
Ryan Moore <ryan.moore@grandrounds.com>
2017-02-13 09:37:05 -0800

    update drive.py to work

--- project3/drive.py
+++ project3/drive.py
@@ -12,13 +12,13 @@ from PIL import ImageOps
 from flask import Flask, render_template
 from io import BytesIO
 
-from keras.models import model_from_json
+from keras.models import load_model
+
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
 
 # Fix error with Keras and TensorFlow
 import tensorflow as tf
 tf.python.control_flow_ops = tf
-
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -62,21 +62,10 @@ def send_control(steering_angle, throttle):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Remote Driving')
     parser.add_argument('model', type=str,
-    help='Path to model definition json. Model weights should be on the same path.')
+    help='Path to model definition h5. Model should be on the same path.')
     args = parser.parse_args()
-    with open(args.model, 'r') as jfile:
-        # NOTE: if you saved the file by calling json.dump(model.to_json(), ...)
-        # then you will have to call:
-        #
-        #   model = model_from_json(json.loads(jfile.read()))\
-        #
-        # instead.
-        model = model_from_json(jfile.read())
 
-
-    model.compile("adam", "mse")
-    weights_file = args.model.replace('json', 'h5')
-    model.load_weights(weights_file)
+    model = load_model(args.model)
 
     # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
