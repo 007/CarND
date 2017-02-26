628a656565ec4414890a1cfcaf0a5eeefa0ee1d7
Ryan Moore <ryan@geekportfolio.com>
2017-02-25 22:54:01 -0800

    25 epochs makes it past the dirt road

--- project3/model.py
+++ project3/model.py
@@ -2,7 +2,7 @@
 
 AUGMENT_ANGLE = 0.33 # angle offset for L/R images
 BATCH_SIZE = 32
-EPOCHS = 10
+EPOCHS = 25
 INPUT_SHAPE = (160,320,3) # TF ordering, not TH ordering - all class docs seem to get this wrong?
 LEARNING_RATE = 0.01
 
