2545330a8373b3740f3853d177ae5a4eafbad3c4
Ryan Moore <ryan@geekportfolio.com>
2017-03-12 21:30:18 -0700

    modularize each helper function

--- project4/local_debug_helper.py
+++ project4/local_debug_helper.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-import matplotlib.pyplot as plt
-# helper function for debugging - write image to plot and (blocking) show immediately
+# helper function for debugging - write image to plot and show immediately
 def imgprint(img):
+    import matplotlib.pyplot as plt
     plt.imshow(img)
     plt.show()
 
 def imgprint_h(img_arr):
+    import matplotlib.pyplot as plt
     plot_count = len(img_arr)
     fig = plt.figure()
     for i in range(plot_count):
