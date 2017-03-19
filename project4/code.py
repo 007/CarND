468350518cb51468607cf6ef9702b8851c0bb77e
Ryan Moore <ryan@geekportfolio.com>
2017-03-19 10:06:05 -0700

    re-use windowing function results between runs

--- project4/code.py
+++ project4/code.py
@@ -116,8 +116,10 @@ def perspective_unwarp_lane(img):
     warped = cv2.warpPerspective(img, M, img_size, flags=cv2.WARP_INVERSE_MAP)
     return warped
 
+first_run = True
 """ Detect lane pixels and fit to find the lane boundary. """
 def find_lane_lines(img):
+    global first_run, left_fit, right_fit
     histogram = np.sum(img, axis=0)
 
     # Assuming you have created a warped binary image called "img"
@@ -138,42 +140,54 @@ def find_lane_lines(img):
     nonzero = img.nonzero()
     nonzeroy = np.array(nonzero[0])
     nonzerox = np.array(nonzero[1])
-    # Current positions to be updated for each window
-    leftx_current = leftx_base
-    rightx_current = rightx_base
+
     # Set the width of the windows +/- margin
     margin = 100
     # Set minimum number of pixels found to recenter window
     minpix = 50
-    # Create empty lists to receive left and right lane pixel indices
-    left_lane_inds = []
-    right_lane_inds = []
 
-    # Step through the windows one by one
-    for window in range(nwindows):
-        # Identify window boundaries in x and y (and right and left)
-        win_y_low = IMG_H - (window+1)*window_height
-        win_y_high = IMG_H - window*window_height
-        win_xleft_low = leftx_current - margin
-        win_xleft_high = leftx_current + margin
-        win_xright_low = rightx_current - margin
-        win_xright_high = rightx_current + margin
-        # Draw the windows on the visualization image
-        # Identify the nonzero pixels in x and y within the window
-        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
-        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
-        # Append these indices to the lists
-        left_lane_inds.append(good_left_inds)
-        right_lane_inds.append(good_right_inds)
-        # If you found > minpix pixels, recenter next window on their mean position
-        if len(good_left_inds) > minpix:
-            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
-        if len(good_right_inds) > minpix:
-            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
 
-    # Concatenate the arrays of indices
-    left_lane_inds = np.concatenate(left_lane_inds)
-    right_lane_inds = np.concatenate(right_lane_inds)
+    if first_run:
+        first_run = False
+        # Current positions to be updated for each window
+        leftx_current = leftx_base
+        rightx_current = rightx_base
+        # Create empty lists to receive left and right lane pixel indices
+        left_lane_inds = []
+        right_lane_inds = []
+
+        # Step through the windows one by one
+        for window in range(nwindows):
+            # Identify window boundaries in x and y (and right and left)
+            win_y_low = IMG_H - (window+1)*window_height
+            win_y_high = IMG_H - window*window_height
+            win_xleft_low = leftx_current - margin
+            win_xleft_high = leftx_current + margin
+            win_xright_low = rightx_current - margin
+            win_xright_high = rightx_current + margin
+            # Draw the windows on the visualization image
+            # Identify the nonzero pixels in x and y within the window
+            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+            # Append these indices to the lists
+            left_lane_inds.append(good_left_inds)
+            right_lane_inds.append(good_right_inds)
+            # If you found > minpix pixels, recenter next window on their mean position
+            if len(good_left_inds) > minpix:
+                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+            if len(good_right_inds) > minpix:
+                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+        # Concatenate the arrays of indices
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+    else:
+        # Assume you now have a new warped binary image
+        # from the next frame of video (also called "binary_warped")
+        # It's now much easier to find line pixels!
+        left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] + margin)))
+        right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] + margin)))
+
 
     # Extract left and right line pixel positions
     leftx = nonzerox[left_lane_inds]
@@ -263,15 +277,15 @@ def pipeline(input_image):
     warped = perspective_warp_lane(threshold)
 #    cv2.imwrite('./output/traffic_perspective.png', warped * 255) # binary * 255 = black/white
 
-    left_fit, right_fit, curve, center = find_lane_lines(warped)
+    left_poly, right_poly, curve, center = find_lane_lines(warped)
 
-    overlay = build_lane_overlay(warped, left_fit, right_fit)
+    overlay = build_lane_overlay(warped, left_poly, right_poly)
     final = output_with_overlays(corrected, overlay, curve, center)
     return final
 
 def video_pipeline(input_video):
     from moviepy.editor import VideoFileClip
-    in_vid = VideoFileClip(input_video).subclip(45)
+    in_vid = VideoFileClip(input_video)
     out_vid = in_vid.fl_image(pipeline)
     out_vid.write_videofile('./output/annotated_video.mp4', audio=False)
     play_vid('output/annotated_video.mp4')
