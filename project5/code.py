a0768b67e8a7166b65d7eb583c01e52ed6c91b1e
Ryan Moore <ryan@geekportfolio.com>
2017-03-19 22:48:11 -0700

    better timing code

--- project5/code.py
+++ project5/code.py
@@ -154,17 +154,16 @@ if __name__ == '__main__':
     # Use a linear SVC
     svc = LinearSVC()
     # Check the training time for the SVC
-    t=time.time()
+    time_start = time.time()
     svc.fit(X_train, y_train)
-    t2 = time.time()
-    print(round(t2-t, 2), 'Seconds to train SVC...')
+    time_end = time.time()
+    print(round(time_end - time_start, 2), 'Seconds to train SVC...')
     # Check the score of the SVC
     print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
     # Check the prediction time for a single sample
-    t=time.time()
+    time_start = time.time()
     n_predict = 10
     print('    My SVC predicts: ', svc.predict(X_test[0:n_predict]))
     print('For these',n_predict, 'labels: ', y_test[0:n_predict])
-    t2 = time.time()
-    print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
-
+    time_end = time.time()
+    print(round(time_end - time_start, 5), 'Seconds to predict', n_predict,'labels with SVC')
