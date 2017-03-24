1b2a7926f7c179c3482ad8a6bde728d2216782b7
Ryan Moore <ryan@geekportfolio.com>
2017-03-23 23:21:11 -0700

    split mini-train and hog functions

---
+++ project5/hog.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+from skimage.feature import hog
+
+# tweaked params from suggested 9/8/2 to 18/6/3 based on http://slideplayer.com/slide/5111258/ slide 19
+HOG_ORIENTATIONS = 18
+HOG_CELL_SIZE = 6
+HOG_CELLS_PER_BLOCK = 3
+
+def get_hog_features(img):
+    features = hog( img,
+                    orientations=HOG_ORIENTATIONS,
+                    pixels_per_cell=(HOG_CELL_SIZE, HOG_CELL_SIZE),
+                    cells_per_block=(HOG_CELLS_PER_BLOCK, HOG_CELLS_PER_BLOCK),
+                    transform_sqrt=True,
+                    visualise=False,
+                    feature_vector=True
+                    )
+    return features
+
+def process_image_hog(img):
+    # account for stupid mpimg.imread that scales things differently
+    if np.ptp(img) <= 1.: img = (img * 255.).astype(np.uint8)
+    feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    hog_features = []
+    for channel in range(feature_image.shape[2]):
+        hog_features.append(get_hog_features(feature_image[:,:,channel]))
+    return np.ravel(hog_features)
+
---
+++ project5/train.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import matplotlib.image as mpimg
+import numpy as np
+import glob
+import pickle
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+from hog import process_image_hog
+
+# Define a function to extract features from a list of images
+def extract_hog_features(imgs):
+    features = []
+    for file in imgs:
+        image = mpimg.imread(file)
+        features.append(process_image_hog(image))
+    return features
+
+if __name__ == '__main__':
+    cars = glob.glob('data/vehicles/**/*.png', recursive=True)
+    not_cars = glob.glob('data/non-vehicles/**/*.png', recursive=True)
+
+    car_features = extract_hog_features(cars)
+    notcar_features = extract_hog_features(not_cars)
+
+    # Create an array stack of feature vectors
+    X = np.vstack((car_features, notcar_features)).astype(np.float64)
+
+    # Fit a per-column scaler
+    X_scaler = StandardScaler().fit(X)
+
+    # Apply the scaler to X
+    scaled_X = X_scaler.transform(X)
+
+    # Define the labels vector
+    y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+    # Split up data into randomized training and test sets
+    rand_state = 4 # chosen by fair dice roll, guaranteed to be random
+    X_train, X_test, y_train, y_test = train_test_split(
+        scaled_X, y, test_size=0.2, random_state=rand_state)
+
+    # Use a linear SVC
+    svc = LinearSVC()
+    svc.fit(X_train, y_train)
+    predict = svc.predict(X_test)
+
+    # count false positives, negatives, exact matches
+    false_positives = np.zeros_like(predict)
+    false_positives[(predict == 1) & (y_test == 0)] = 1
+    pos_count = np.sum(false_positives)
+
+    false_negatives = np.zeros_like(predict)
+    false_negatives[(predict == 0) & (y_test == 1)] = 1
+    neg_count = np.sum(false_negatives)
+
+    matches = np.zeros_like(predict)
+    matches[predict == y_test] = 1
+    match_count = np.sum(matches)
+
+    print(  'accuracy is', round(svc.score(X_test, y_test) * 100, 3),
+            ', false positives', pos_count,
+            ', false negatives', neg_count,
+            ', matches', match_count, 'out of', len(X_test)
+        )
+
+    # Save and reload to confirm it'll work IRL
+    with open('svm.p', 'wb') as f:
+        pickle.dump(svc, f)
+
